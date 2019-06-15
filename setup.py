925b5012d36f7406ecdb971ecf93f5c7134c2fc9
Ravisankar <rasa97uchiha@gmail.com>
2019-06-15 12:24:54 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-   name='qAuth',
+   name='qAuth-rasa97',
    version='0.0.1',
    description='Quantum Authentication Protocols libraries',
+   author_email="rasa97uchiha@gmail.com"
    author='Ravisankar A V',
    packages=find_packages(),
    include_package_data=True,
-   install_requires=['simulaqron', 'cqc'],
+   install_requires=['simulaqron==2.1.1', 'cqc==2.1.0'],
    zip_safe=False
 )
